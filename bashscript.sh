f43fde48d18e5485153cbae82ced4224af633290
gitsysop <lluuxxoorr@gmail.com>
2022-07-01 19:01:34 +0200

    edit

---
+++ bashscript.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Connect and set Subscription Context in Azure
+az login
+az account set --subscription "Azure subscription 1"
+
+
+#Set Variables for Storage account and Key Vault that support the Terraform implementation
+RESOURCE_GROUP_NAME=cloudemerge-infra
+STORAGE_ACCOUNT_NAME=cloudemerge-sa
+CONTAINER_NAME=cloudemerge-container
+STATE_FILE="terraform.state"
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location euwest
+
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Get storage account key (Only used if SPN not available)
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+
+# Show details for the purposes of this code
+echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
+echo "container_name: $CONTAINER_NAME"
+echo "access_key: $ACCOUNT_KEY"
+echo "state_file: $STATE_FILE"
+
+# Create KeyVault and example of storing a key
+az keyvault create --name "cloudemerge-kv" --resource-group "cloudemerge-infra-rg" --location "westeurope"
+az keyvault secret set --vault-name "cloudemerge-kv" --name "cloudemergecontainer-access" --value {$ACCOUNT_KEY}
+az keyvault secret show --vault-name "cloudemerge-kv" --name "cloudemergecontainer-access"
---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloudemerge-infra"
+    storage_account_name = "cloudemergecontainer"
+    container_name       = "cloudemergecontainer-container"
+    key                  = "Iw6QVpbEinx+2kygBdMi+eDsl4AMZwbBbL4kht/GaZA4THO2o33dbiwxTk0gjTx52aomY5Zlio9p+AStWqA/HA=="
+  }
+
+  required_providers {
+    azurerm = {
+      # Specify what version of the provider we are going to utilise
+      source  = "hashicorp/azurerm"
+      version = ">= 2.4.1"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+data "azurerm_client_config" "current" {}
+# Create our Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "cloudemerge-app01"
+  location = "europe west"
+}
+# Create our Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "cloudemerge-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+# Create our Subnet to hold our VM - Virtual Machines
+resource "azurerm_subnet" "sn" {
+  name                 = "VM"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+# Create our Azure Storage Account - jonnychipzsa
+resource "azurerm_storage_account" "cloudemergecontainer" {
+  name                     = "cloudemergecontainer"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "cloudemerge-env1"
+  }
+}
+# Create our vNIC for our VM and assign it to our Virtual Machines Subnet
+resource "azurerm_network_interface" "vmnic" {
+  name                = "cloudemerge-vm01nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+# Create our Virtual Machine
+resource "azurerm_virtual_machine" "cloudemerge-vm01" {
+  name                  = "cloudemerge-vm01"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vmnic.id]
+  vm_size               = "Standard_B2s"
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter-Server-Core-smalldisk"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "cloudemerge-vm01os"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "cloudemerge-vm01"
+    admin_username = "cloudadmin"
+    admin_password = "Password123$"
+  }
+  os_profile_windows_config {
+  }
+}
